39da68e127cf0dcf45575242dcbf9c6844cdeb6e
awei0905 <awei0905@gmail.com>
2015-09-08 23:33:44 +0800

    Too late... go sleep...
    zzz...

--- Industry Techniques Competition/ITC102/Answer/ITC102-1/ITC102-1/Form1.Designer.cs
+++ Industry Techniques Competition/ITC102/Answer/ITC102-1/ITC102-1/Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "工作桌編號 問號 選手名稱 你猜猜看 代表學校 雙葉幼稚園";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 250);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1.輸入資料";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(318, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(150, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2.權重遮罩設定";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(474, 37);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(150, 100);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "3.輸入要運算的3x3區域之左上角座標";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(711, 37);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(150, 100);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "5.編碼結果";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "4.計算";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 267);
+            this.ClientSize = new System.Drawing.Size(876, 297);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "全國高級中等學校102學年度工業類科學生技藝競賽";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Industry Techniques Competition/ITC102/Answer/ITC102-1/ITC102-1/Form1.cs
+++ Industry Techniques Competition/ITC102/Answer/ITC102-1/ITC102-1/Form1.cs
@@ -12,9 +12,50 @@ namespace ITC102_1
 {
     public partial class Form1 : Form
     {
+        private TextBox[,] arrayOfEnterData, arrayOfWeight, arrayOfTopLeft;
+
         public Form1()
         {
             InitializeComponent();
+            this.arrayOfEnterData = new TextBox[6, 6];
+            this.arrayOfWeight = new TextBox[3, 3];
+            this.arrayOfTopLeft = new TextBox[2, 1];
+            FillTextBoxToArray(this.arrayOfEnterData);
+            FillTextBoxToArray(this.arrayOfWeight);
+            FillTextBoxToArray(this.arrayOfTopLeft);
+            SetTextBoxArrayLocation(this.arrayOfEnterData);
+            SetTextBoxArrayLocation(this.arrayOfWeight);
+            SetTextBoxArrayLocation(this.arrayOfTopLeft);
         }
+
+        public void FillTextBoxToArray(TextBox[,] textBoxes)
+        {
+            int oneDimension, twoDimension;
+            oneDimension = textBoxes.GetLength(0);
+            twoDimension = textBoxes.GetLength(1);
+            for(int i = 0; i < oneDimension; i++)
+            {
+                for(int j = 0; j < twoDimension; j++)
+                {
+                    textBoxes[i, j] = new TextBox();
+                }
+            }
+        }
+
+        public void SetTextBoxArrayLocation(TextBox[,] textBoxes)
+        {
+            int oneDimension, twoDimension;
+            oneDimension = textBoxes.GetLength(0);
+            twoDimension = textBoxes.GetLength(1);
+            for(int i = 0; i < oneDimension; i++)
+            {
+                for(int j = 0; j < twoDimension; j++)
+                {
+                    textBoxes[i, j].Location = new Point(10 + i * 32, 20 + j * 32);
+                }
+            }
+        }
+
+        
     }
 }
